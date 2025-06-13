db608e55b653787a37816d5301f3f73b88945d82
ADR-ian <119506352+ADR-ian-ba@users.noreply.github.com>
2025-06-13 16:19:39 +0700

    added cord for nusagitra

--- src/helper/ApiHelper.js
+++ src/helper/ApiHelper.js
@@ -121,6 +121,7 @@ class ApiHelper {
 }
 
 // const apiHelper = new ApiHelper("http://localhost:3000")
-const apiHelper = new ApiHelper("https://streamhaven-be.onrender.com")
+// const apiHelper = new ApiHelper("https://streamhaven-be.onrender.com")
+const apiHelper = new ApiHelper("https://streamhaven-be.nusagitra.web.id")
 
 export default apiHelper
